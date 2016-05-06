f28fb1646184435dbe68ba7b9d35974e7fd8c20b
Rafael Romão <rafaelm@takenet.com.br>
2016-05-06 13:15:05 -0300

    fix --help

--- Get-Lines.psm1
+++ Get-Lines.psm1
@@ -2,22 +2,26 @@
 function ParseArguments($input_args) {
 	$result = New-Object System.Object
 	$result | Add-Member -type NoteProperty -name printHelp -value $false
+	$result | Add-Member -type NoteProperty -name input -value $null
 	$result | Add-Member -type NoteProperty -name skip -value $null
 	$result | Add-Member -type NoteProperty -name take -value $null
+	$result | Add-Member -type NoteProperty -name column -value $null
 	$result | Add-Member -type NoteProperty -name match -value $null
 
-	For ($i = 0; $i -lt $input_args.Length; $i++) {
+	$result.input = $input_args[0]
+
+	# Check if shall print help
+	if ($result.input -eq "--help" -or $result.input -eq "-h") {
+		$result.printHelp = $true
+	}
+	
+	For ($i = 1; $i -lt $input_args.Length; $i++) {
 		# Parse the current and next arguments
 		$arg = $input_args[$i]
 		$hasNextArg = $i -lt $input_args.Length-1
 		$nextArg = $null
 		if ($hasNextArg) {
 			$nextArg = $input_args[$i+1]
-		}
-
-		# Check if shall print help
-		if ($arg -eq "--help" -or $arg -eq "-h") { | ge-colum
-			$result.printHelp = $true
 		}
 		
 		# Get the skip parameter
@@ -27,15 +31,19 @@ function ParseArguments($input_args) {
 		
 		# Get the take parameter
 		if ($arg -eq "--take" -or $arg -eq "-t") {
-			$result.skip = "$($nextArg)"
+			$result.take = "$($nextArg)"
+		}
+
+		# Get the take parameter
+		if ($arg -eq "--column" -or $arg -eq "-c") {
+			$result.column = "$($nextArg)"
 		}
 
 		# Get the take parameter
 		if ($arg -eq "--match" -or $arg -eq "-m") {
-			$result.skip = "$($nextArg)"
+			$result.match = "$($nextArg)"
 		}
 	}
-	
 	return $result
 }
 
@@ -50,26 +58,28 @@ function CheckIfMustPrintHelp($printHelp) {
 		Write-Host ""
 		Write-Host "--skip 1 `t`t`t -s 1 `t`t`t skip the given number of lines"
 		Write-Host "--take 2 `t`t`t -t 2 `t`t`t take the given number of lines"
-		Write-Host "--match ^(word)& `t`t`t -m ^(word)&`t`t`t skip lines that do not match the regex"
+		Write-Host "--column 3 `t`t`t -c 3 `t`t`t return only the third column"
+		Write-Host "--match ^(word)& `t`t -m ^(word)&`t`t`t skip lines that do not match the regex"
 		Write-Host ""
-		exit
 	}
 }
 
 # Get the lines that match the criterea and write to output
 function GetLines($input, $skip, $take, $regex) {
 	$result = get-content $input | select-object -skip 1
-	if ($skip != $null) {
-		$result = $result  | select-object -skip $skip
+	if ($skip -ne $null) {
+		$result = $result | select-object -skip $skip
 	}
-	if ($take != $null) {
-		$result = $result  | select-object -first $take
+	if ($take -ne $null) {
+		$result = $result | select-object -first $take
 	}
-	if ($regex != $null) {
-		$result = $result  | Where-Object {$_.Name -match $regex}
+	if ($regex -ne $null) {
+		$result = $result | select-string -pattern $regex
 	}
-	
-	Write-Host -foregroundcolor Yellow "Executing mono $($nunitConsole) ""$($testAssembly)"" --framework=mono-4.0 $($arguments)"
+	if ($column -ne $null) {
+		$result = get-object $result | select -ExpandProperty P$column
+	}
+	Write-Host $result
 }
 
 function Get-Lines() {
@@ -77,6 +87,8 @@ function Get-Lines() {
 	$arguments = ParseArguments $args
 	# Check if the arguments used require the help to be printed
 	CheckIfMustPrintHelp $arguments.printHelp
-	# Process input file
-	GetLines $input $arguments.skip $arguments.skip $arguments.regex
+	if (!$arguments.printHelp) {
+		# Process input file
+		GetLines $arguments.input $arguments.skip $arguments.take $arguments.regex $arguments.column
+	}
 }
